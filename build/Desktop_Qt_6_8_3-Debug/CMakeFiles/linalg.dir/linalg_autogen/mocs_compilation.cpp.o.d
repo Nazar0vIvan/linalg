d1f4a258f83a99049e825f528e25fda4e3ec1c27
BormachBoroda <vano888888@gmail.com>
2025-11-05 23:50:57 +0300

    chore: not essential changes

--- build/Desktop_Qt_6_8_3-Debug/CMakeFiles/linalg.dir/linalg_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/linalg.dir/linalg_autogen/mocs_compilation.cpp.o: \
- /home/boroda/Qt/QtProjects/linalg/build/Desktop_Qt_6_8_3-Debug/linalg_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
